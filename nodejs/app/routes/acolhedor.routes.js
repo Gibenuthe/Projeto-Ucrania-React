f6bef6e18da9102d73f1697a5ff84cab5f58399b
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2022-11-30 19:47:41 -0300

    Arrumando rotas

--- nodejs/app/routes/acolhedor.routes.js
+++ nodejs/app/routes/acolhedor.routes.js
@@ -1,25 +1,25 @@
 module.exports = app => {
-    const tutorials = require("../controllers/acolhedor.controller.js");
+    const acolhedores = require("../controllers/acolhedor.controller.js");
   
     var router = require("express").Router();
   
     // Cria um acolhedor
-    router.post("/", tutorials.create);
+    router.post("/", acolhedores.create);
   
     // Busca todos acolhedores
-    router.get("/", tutorials.findAll);
+    router.get("/", acolhedores.findAll);
   
     // Busca acolhedor pelo id
-    router.get("/:id", tutorials.findOne);
+    router.get("/:id", acolhedores.findOne);
   
     // Atualiza acolhedor pelo id
-    router.put("/:id", tutorials.update);
+    router.put("/:id", acolhedores.update);
   
     // Deleta acolhedor pelo id
-    router.delete("/:id", tutorials.delete);
+    router.delete("/:id", acolhedores.delete);
   
     // Cria novo acolhedor
-    router.delete("/", tutorials.deleteAll);
+    router.delete("/", acolhedores.deleteAll);
   
     app.use("/api/acolhedores", router);
   };
