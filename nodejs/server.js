e6e0ec80dd5829be32d8e31550402cdf1e306329
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2022-11-30 20:42:26 -0300

    Conexão front-back

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require('dotenv').config()
-console.log(process.env)
-// const path = __dirname + '/app/views/';
+
+// console.log(process.env)
+const path = __dirname + '/app/views/';
 const app = express();
 
-// app.use(express.static(path));
+app.use(express.static(path));
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -38,10 +39,11 @@ db.mongoose
 
 // rota simples
 app.get("/", (req, res) => {
-  res.json({ message: "Bem Vindo ao Projeto Ucrania" });
+  // res.json({ message: "Bem Vindo ao Projeto Ucrania" });
+  res.sendFile(path + "index.html");
 });
 
-// require("./app/routes/acolhedor.routes")(app);
+require("./app/routes/acolhedor.routes")(app);
 
 // escolhe porta pra receber as requisições
 const PORT = process.env.PORT || 8080;
